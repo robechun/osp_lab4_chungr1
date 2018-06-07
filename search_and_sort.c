df171002785b244bc54554cbdfe1697a85b86f4f
Robert Chung <robertchung97@gmail.com>
2018-06-07 01:58:56 -0700

    Found memory bug. Resizing dynamic array now; bug with free() of original list. Commented majority of debug messages. Trying to make extending the list work.

--- search_and_sort.c
+++ search_and_sort.c
@@ -3,18 +3,19 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
-#include <sys/time.h>
+#include <time.h>
 #include <errno.h>
 
 //takes a file/dir as argument, recurses,
 // prints name if empty dir or not a dir (leaves)
 void recur_file_search(char *file);
-void printList();
+void printList(char**);
+void extendList(char**);
 
-//share search term globally (rather than passing recursively)
-const char *search_term;
+const char *search_term;    // share_term for global use instead of passing in
 char **allFiles;			// array of all files found
 int count = 0;				// global counter for above list
+int allFiles_cap = 10;		// A changing cap for the "vector" that is allFiles
 
 int main(int argc, char **argv)
 {
@@ -40,9 +41,8 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 	
-	allFiles = malloc(sizeof(char*));
+	allFiles = malloc(sizeof(char*) * allFiles_cap);
 	char *argv_2_cpy = strdup(argv[2]);
-	// TODO, might need to strdup argv[2] for passing in
 	clock_t start = clock(), diff;
 	recur_file_search(argv_2_cpy);
 	diff = clock() - start;
@@ -51,7 +51,7 @@ int main(int argc, char **argv)
 
 	printf("Time taken %d seconds %d milliseconds\n", msec/1000, msec%1000);
 
-	printList();
+	printList(allFiles);
 
 	return 0;
 }
@@ -72,7 +72,13 @@ void recur_file_search(char *file)
 	//check if directory is actually a file
 	DIR *d = opendir(file);
 
-	printf("DEBUG:recursed--%s\n", file);
+	// If we need to allocate more memory to make space for the new files
+	// that might be added, extend the allFiles list.
+	if (count == allFiles_cap) {
+		extendList(allFiles);
+	}
+
+	//printf("DEBUG:recursed--%s\n", file);
 	//NULL means not a directory (or another, unlikely error)
 	if(d == NULL)
 	{
@@ -87,37 +93,47 @@ void recur_file_search(char *file)
 		}
 
 		//nothing weird happened, so add to list of found paths
-		printf("DEBUG:_NOT a directory\n");
+		//printf("DEBUG:_NOT a directory\n");
 		allFiles[count] = malloc(sizeof(char*));
 		allFiles[count] = file;
 		count++;
-		printf("DEBUG: After insert file_only\n");
-		printf("DEBUG:  INSERTED:%s\n", allFiles[count-1]);
 
+		//printf("DEBUG: After insert file_only\n");
+		//printf("DEBUG:  INSERTED:%s\n", allFiles[count-1]);
+
+		//printf("-------- START PRINT LIST --------------\n");
+		printList(allFiles);
+		//printf("------- END PRINT LIST -----------------\n");
 		//no need to close d (we can't, it is NULL!)
 		return;
 	}
 
 
-	printf("DEBUG:_IS a directory\n");
+	//printf("DEBUG:_IS a directory\n");
 	//we have a directory, so add "/" onto the end and put it in the list
 	char *file_cpy_dir = malloc(sizeof(char) * strlen(file) + 1);
 	strncpy(file_cpy_dir, file, strlen(file));
 	strncpy(file_cpy_dir + strlen(file), "/", 1);
 
-	printf("DEBUG: cpy is:%s\n", file_cpy_dir);
+	//printf("DEBUG: cpy is:%s\n", file_cpy_dir);
 
 	allFiles[count] = malloc(sizeof(char*));
 	allFiles[count] = file_cpy_dir;
 	count++;
-	printf("DEBUG:  INSERTED:%s\n", allFiles[count-1]);
 
+	//printf("DEBUG:  INSERTED:%s\n", allFiles[count-1]);
+
+	printf("-------- START PRINT LIST --------------\n");
+	printList(allFiles);
+	printf("------- END PRINT LIST -----------------\n");
 	//call recur_file_search for each file in d
 	//readdir "discovers" all the files in d, one by one and we
 	// recurse on those until we run out (readdir will return NULL)
 	struct dirent *cur_file;
 	while((cur_file = readdir(d)) != NULL)
 	{
+		//printf("Hello:%s\n",file);
+		//printf("DEBUG: cur_file is:%s\n", cur_file->d_name);
 		//make sure we don't recurse on . or ..
 		if(strcmp(cur_file->d_name, "..") != 0 &&\
 				strcmp(cur_file->d_name, ".") != 0)
@@ -130,7 +146,7 @@ void recur_file_search(char *file)
 					strlen(cur_file->d_name) + \
 					strlen(file) + 2);
 
-			printf("DEBUG: FILE is:%s\n", file);
+			//printf("DEBUG: FILE is:%s\n", file);
 			strncpy(next_file_str, file, strlen(file));
 			strncpy(next_file_str + strlen(file), \
 					"/", 1);
@@ -138,27 +154,52 @@ void recur_file_search(char *file)
 					cur_file->d_name, \
 					strlen(cur_file->d_name) + 1);
 
+			//printf("DEBUG: FILE is:%s\n", file);
+			//printf("DEBUG: next_file is:%s\n", next_file_str);
+
 			//recurse on the file
 			recur_file_search(next_file_str);
-			printf("DEBUG: *** returned from recursion ***\n");
+			//printf("DEBUG: *** returned from recursion ***\n");
+			//printf("hello2:%s\n", file);
 			//free the dynamically-allocated string
 			//free(next_file_str);
 		}
-		printf("DEBUG: cur_file is:%s\n", cur_file->d_name);
 	}
-	printf("DEBUG: ---- end of while loop ---\n");
+	//printf("DEBUG: ---- end of while loop ---\n");
 
-	//free(file);
+	free(file);
 	//close the directory, or we will have too many files opened (bad times)
 	closedir(d);
 }
 
 
-void printList() {
+// printList simply prints out the contents of the string array (char **) passed
+void printList(char **list) {
 
 	for (int i = 0; i < count; i++)
 	{
-		printf("%s", allFiles[i]);
+		printf("%s\n", list[i]);
 	}
 }
 
+// extendList is to resize the dynamic array passed in.
+// In this case, we are increasing the size of a char **list by 2x.
+// This allows for more items to be stored in the dynamic array
+void extendList(char **list) {
+
+	allFiles_cap = allFiles_cap * 2;	// increase the cap by 2x
+
+
+	char **temp = realloc(list, sizeof(char*) * allFiles_cap);
+
+	// Something went wrong with realloc, so exit
+	if (!temp) {
+		perror("Reallocating failed!\n");
+		exit(1);
+	}
+
+	free(list);
+	list = temp;
+	
+	printList(list);
+}
